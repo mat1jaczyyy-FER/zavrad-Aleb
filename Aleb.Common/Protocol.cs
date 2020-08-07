01a7ca82ec8c9aaa12fcc172d7bd3a88b01eab1a
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-08-07 03:41:36 +0200

    Bump version number

--- Aleb.Common/Protocol.cs
+++ Aleb.Common/Protocol.cs
@@ -2,7 +2,7 @@
     public static class Protocol {
         public const string Localhost = "localhost";
         public const int Port = 11252;
-        public const int Version = 6;
+        public const int Version = 7;
         public const char Delimiter = ';';
         public const char ListDelimiter = '|';
     }
